aac4f67384022a29bd2568bb44a41e374815adc2
SubajananiSwaminathan <subajanani@gmail.com>
2024-07-12 12:38:06 +0530

    Creating Components and Styles

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,20 +1,32 @@
-import styles from "./Homepage.module.css";
+// import styles from "./Homepage.module.css";
 
-export default function Homepage() {
+import PageNav from "../components/PageNav";
+
+// export default function Homepage() {
+//   return (
+//     <main className={styles.homepage}>
+//       <section>
+//         <h1>
+//           You travel the world.
+//           <br />
+//           WorldWise keeps track of your adventures.
+//         </h1>
+//         <h2>
+//           A world map that tracks your footsteps into every city you can think
+//           of. Never forget your wonderful experiences, and show your friends how
+//           you have wandered the world.
+//         </h2>
+//       </section>
+//     </main>
+//   );
+// }
+function Homepage() {
   return (
-    <main className={styles.homepage}>
-      <section>
-        <h1>
-          You travel the world.
-          <br />
-          WorldWise keeps track of your adventures.
-        </h1>
-        <h2>
-          A world map that tracks your footsteps into every city you can think
-          of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
-        </h2>
-      </section>
-    </main>
+    <div>
+      <PageNav/>
+      <h1>Homepage</h1>
+    </div>
   );
 }
+
+export default Homepage;
